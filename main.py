8db3b5e54525f09e37b89c37f9e865bcd3021cbb
Loïc Penaud <l.penaud@zaclys.net>
2018-04-24 10:36:20 +0200

    Add SQL script to create database

--- main.py
+++ main.py
@@ -5,33 +5,6 @@ from src.Sqlite import Database
 
 db = Database('sql/mydb.db')
 
-db.execute_from_file('sql/sample.sql')
-
-data_to_insert = [
-    {
-        'name': 'ypenaud',
-        'age': 22
-    },
-    {
-        'name': 'mpenaud',
-        'age': 25
-    }
-]
-
-print('INSERT')
-db.insertmany('users', data_to_insert)
-data = db.selectall('SELECT * FROM users')
-for value in data:
-    print(value)
-
-data_to_update = data_to_insert[0].copy()
-data_to_update['name'] = 'freddy'
-data_to_update['age'] = 20
-
-print('UPDATE')
-db.update('users', data_to_update, 'id=1 OR id=3')
-data = db.selectall('SELECT * FROM users')
-for value in data:
-    print(value)
+db.execute_from_file('sql/RPG.sql')
 
 db.close()
